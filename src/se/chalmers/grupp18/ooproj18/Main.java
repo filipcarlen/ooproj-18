6dfb5561bd981e75bf29c703f91558d692c417ba
Marcus Lagerstedt <lager93@gmail.com>
2013-03-19 14:59:33 +0100

    Change

--- src/se/chalmers/grupp18/ooproj18/Main.java
+++ src/se/chalmers/grupp18/ooproj18/Main.java
@@ -3,7 +3,7 @@ package se.chalmers.grupp18.ooproj18;
 public class Main {
 	
 	public static void main(String[] args){
-		System.out.println("Hello World!");
+		System.out.println("Hello Warld!");
 		System.out.println("Test");
 	}
 
