bcf454fd95738a9828db7b04fa3c6473d225e32f
Filip Carlen <filipcarlen@gmail.com>
2013-04-15 15:02:53 +0200

    Main Class

---
+++ src/se/chalmers/grupp18/ooproj18/Main.java
@@ -0,0 +1,13 @@
+package se.chalmers.grupp18.ooproj18;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
