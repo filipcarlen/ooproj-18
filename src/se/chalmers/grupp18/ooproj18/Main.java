64e7b8941254eff0644b1d9b0dff000d976b1d70
elinljunggren <elin.l.ljunggren@gmail.com>
2013-04-15 15:06:31 +0200

    Fix

--- src/se/chalmers/grupp18/ooproj18/Main.java
+++ src/se/chalmers/grupp18/ooproj18/Main.java
@@ -1,26 +1,11 @@
 package se.chalmers.grupp18.ooproj18;
 
 public class Main {
-<<<<<<< HEAD
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-=======
-	
-	public static void main(String[] args){
-		System.out.println("HAI");
-		System.out.println("Hellp Warld!");
-		System.out.println("Test");
-<<<<<<< HEAD
-		System.out.println("Linnea");
-=======
-		System.out.println("Elin");
->>>>>>> test1
->>>>>>> test1
 	}
 
 }
