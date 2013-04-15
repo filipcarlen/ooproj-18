77525613de904f2483c054512fd35079c61eeb9a
elinljunggren <elin.l.ljunggren@gmail.com>
2013-04-15 11:40:23 +0200

    test1

--- src/se/chalmers/grupp18/ooproj18/Main.java
+++ src/se/chalmers/grupp18/ooproj18/Main.java
@@ -1,6 +1,7 @@
 package se.chalmers.grupp18.ooproj18;
 
 public class Main {
+<<<<<<< HEAD
 
 	/**
 	 * @param args
@@ -8,6 +9,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+=======
+	
+	public static void main(String[] args){
+		System.out.println("HAI");
+		System.out.println("Hellp Warld!");
+		System.out.println("Test");
+<<<<<<< HEAD
+		System.out.println("Linnea");
+=======
+		System.out.println("Elin");
+>>>>>>> test1
+>>>>>>> test1
 	}
 
 }
