ec84a0a1486b8d6aa6325b0484d3c0bcd15a26f0
Filip Carlen <filipcarlen@gmail.com>
2013-04-15 14:51:50 +0200

    Revert "TEST"
    This reverts commit af7f5e3037b346e023d454029f303947bcd147e5.

--- src/se/chalmers/grupp18/ooproj18/Main.java
+++ src/se/chalmers/grupp18/ooproj18/Main.java
@@ -3,7 +3,7 @@ package se.chalmers.grupp18.ooproj18;
 public class Main {
 	
 	public static void main(String[] args){
-		System.out.println("HAI");
+		
 		System.out.println("Hellp Warld!");
 		System.out.println("Test");
 	}
