b93abf1306cfa092c16b0209fd684d84d16bf92d
Filip Carlen <filipcarlen@gmail.com>
2013-04-15 15:07:36 +0200

    fix 2

--- src/se/chalmers/grupp18/ooproj18/Main.java
+++ src/se/chalmers/grupp18/ooproj18/Main.java
@@ -5,13 +5,9 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-=======
 		
 		System.out.println("hello");
 
->>>>>>> fix
 	}
 
 }
