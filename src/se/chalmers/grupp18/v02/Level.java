581e23ee7167aac81097c1d6a4b7f15b64204c6c
linneaotterlind <linnea.otterlind@gmail.com>
2013-04-17 14:34:49 +0200

    Added v02 with MartteEngine

---
+++ src/se/chalmers/grupp18/v02/Level.java
@@ -0,0 +1,57 @@
+package se.chalmers.grupp18.v02;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import it.marteEngine.Camera;
+import it.marteEngine.CameraFollowStyle;
+import it.marteEngine.World;
+
+
+public class Level extends World{
+
+	private Player hero;
+	
+	public Level(int id, GameContainer container) {
+		super(id, container);
+	}
+	
+	@Override
+	public void init(GameContainer container, StateBasedGame game)
+			throws SlickException {
+		super.init(container, game);
+		
+		hero = new Player(200,200);
+		add(hero);
+		
+		for(int i = 0; i<200000 ; i = i+33){
+			Wall wall = new Wall(i,550);
+			if(!(i==99 || i==132)){
+				add(wall);
+			}
+		}
+		
+		for(int i = 100; i<400 ; i = i+33){
+			Wall wall = new Wall(i,450);
+			//wall.rotateImage();
+			add(wall);
+		}
+		
+		this.camera = new Camera(1920, 1200, 20000, 20000);
+		this.camera.follow(hero, CameraFollowStyle.LOCKON);
+	}
+	
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta)
+			throws SlickException {
+		super.update(container, game, delta);
+	}
+	
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g)
+			throws SlickException {
+		super.render(container, game, g);
+	}
+
+}
---
+++ src/se/chalmers/grupp18/v02/Player.java
@@ -0,0 +1,59 @@
+package se.chalmers.grupp18.v02;
+import it.marteEngine.entity.PlatformerEntity;
+import it.marteEngine.tween.Tweener;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
+
+public class Player extends PlatformerEntity{
+	
+	public Player(float x, float y) throws SlickException {
+		super(x,y,40,48);
+		Image image = new Image("res/toadette.png");
+		setGraphic(image);
+		
+		setHitBox(0,0,image.getWidth(), image.getHeight());
+		addType("PLAYER");
+		
+		bindToKey("RIGHT", Input.KEY_RIGHT);
+		bindToKey("LEFT", Input.KEY_LEFT);
+		bindToKey("JUMP", Input.KEY_UP);
+	}
+	
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		
+		super.update(container, delta);
+		
+		Input input = container.getInput();
+		
+		if(check("LEFT")){
+			if(collide(SOLID,x - 5, y) == null){
+				x -= 0.4f*delta;
+			}
+		}
+		
+		if(check("RIGHT")){
+			if(collide(SOLID,x + 5, y) == null){
+				x += 0.4f*delta;
+			}
+		}
+		
+		if(check("JUMP")){
+			jump();
+		}
+		
+	}
+	
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		super.render(container, g);
+	}
+
+}
