95cf6f26df49ba81865eeb43a4fd0c11e01d56fe
linneaotterlind <linnea.otterlind@gmail.com>
2013-04-19 14:28:03 +0200

    Change IEntityController to contain more useful methods that we think we might use in all entitys.

--- src/se/chalmers/grupp18/v01/IEntityController.java
+++ src/se/chalmers/grupp18/v01/IEntityController.java
@@ -1,7 +1,14 @@
 package se.chalmers.grupp18.v01;
 
+import org.jbox2d.common.Vec2;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
 public interface IEntityController {
 	public void init();
-	public void update();
-	public void render();
+	public void update(GameContainer container, StateBasedGame game, int delta);
+	public void render(GameContainer container, StateBasedGame game, Graphics g);
+	public Vec2 getPosition();
+	public void setPosition(Vec2 pos);
 }
