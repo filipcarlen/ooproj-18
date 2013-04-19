9ed7bdf049a37cc0e592e9d823282502231cc44c
Filip Carlen <filipcarlen@gmail.com>
2013-04-19 14:50:43 +0200

    Havn't got a clue what happend here..
    Removed Collectable, Created Coin class,

---
+++ src/se/chalmers/grupp18/v01/Coin.java
@@ -0,0 +1,43 @@
+package se.chalmers.grupp18.v01;
+
+/** A class representing a collectible coin. The coin has a value (points)
+ *  *  
+ * @author filipcarlen
+ * @version 1.0 
+ */
+
+
+
+public class Coin implements Collectible {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getPoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setPoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
