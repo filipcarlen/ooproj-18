498a97208f30b259a2feb841d64c4da8b0b42210
Filip Carlen <filipcarlen@gmail.com>
2013-04-19 13:54:01 +0200

    Created Interface 'Collectable'

---
+++ src/se/chalmers/grupp18/v01/Collectable.java
@@ -0,0 +1,13 @@
+package se.chalmers.grupp18.v01;
+
+/** An interface representing a collectable item which basicly means
+ *  items that the character can "pick"
+ *  
+ * @author filipcarlen
+ * @version 1.0
+ *
+ */
+
+public interface Collectable {
+
+}
