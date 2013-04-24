bf65605ba1d2b21f86a24fd888ae15a7a4c10c0c
Filip Carlen <filipcarlen@gmail.com>
2013-04-24 20:14:55 +0200

    Created Model, View and Controller for Collectible objects. Refactored code

---
+++ src/se/chalmers/grupp18/v01/CollectibleView.java
@@ -0,0 +1,18 @@
+package se.chalmers.grupp18.v01;
+
+import org.newdawn.slick.Animation;
+
+/**
+ * A class that draw a collectible object
+ * @author filipcarlen
+ *
+ */
+public class CollectibleView {
+	
+	private Animation collectible;
+	
+	public CollectibleView(){
+		
+	}
+
+}
