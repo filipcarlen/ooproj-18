2a103ce825b99ec6c9ad1ed66048ea07efa55fc7
linneaotterlind <linnea.otterlind@gmail.com>
2013-04-25 15:39:49 +0200

    Yet again, I deleted the init() method since we really don't need it.

--- src/se/chalmers/grupp18/v01/IEntityController.java
+++ src/se/chalmers/grupp18/v01/IEntityController.java
@@ -1,12 +1,10 @@
 package se.chalmers.grupp18.v01;
 
-import org.jbox2d.common.Vec2;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.StateBasedGame;
 
 public interface IEntityController {
-	public void init();
 	public void update(GameContainer container, StateBasedGame game, int delta);
 	public void render(GameContainer container, StateBasedGame game, Graphics g);
 }
