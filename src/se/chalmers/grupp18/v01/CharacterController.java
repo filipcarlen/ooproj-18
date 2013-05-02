979860570a8940aebb528cdf9face48e421337de
Marcus Lagerstedt <lager93@gmail.com>
2013-05-02 10:53:49 +0200

    Controller is updated with java 6 and check method is updated

--- src/se/chalmers/grupp18/v01/CharacterController.java
+++ src/se/chalmers/grupp18/v01/CharacterController.java
@@ -1,14 +1,19 @@
 package se.chalmers.grupp18.v01;
 
 import java.util.HashMap;
+
+import org.jbox2d.callbacks.ContactImpulse;
+import org.jbox2d.callbacks.ContactListener;
+import org.jbox2d.collision.Manifold;
 import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.contacts.Contact;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.state.StateBasedGame;
 
 /**
- * Version: 1
+ * Version: 1.0
  * A controller for the Character
  * @author Marcus
  *
@@ -23,6 +28,7 @@ public class CharacterController implements IEntityController{
 	Input input;
 	boolean jump;
 	int jumpCount = 20;
+	int doubleJump = 0;
 	CharacterModel hero;
 	String name;
 	CharacterView pa;
@@ -36,6 +42,9 @@ public class CharacterController implements IEntityController{
 		setControls();
 	}
 	
+	/**
+	 * This is a method to give the default setting to the key inputs
+	 */
 	public void setControls(){
 		bindToKey(CMD_LEFT, Input.KEY_A);
 		bindToKey(CMD_RIGHT, Input.KEY_D);
@@ -43,10 +52,18 @@ public class CharacterController implements IEntityController{
 		bindToKey(CMD_FIGHT, Input.KEY_SPACE);
 	}
 	
+	/**
+	 * this removes all the settings the key bindings
+	 */
 	public void removeKeySettings(){
 		keys.clear();
 	}
 	
+	/**
+	 * here you bind a keys to the different functions
+	 * @param s - the name of a command
+	 * @param key - Gives the integer to the key 
+	 */
 	public void bindToKey(String s, int key){
 		if(s.toLowerCase().equals("left"))
 			keys.put(CMD_LEFT, key);
@@ -60,36 +77,59 @@ public class CharacterController implements IEntityController{
 			System.out.println("Cant name the key like that");
 	}
 	
+	/**
+	 * 
+	 * @param s - the command
+	 * @return true- if the string is binded to a key
+	 * 			false- if thhe string isn't binded to a key
+	 */
 	public boolean check(String s){
-		if(!s.equals(CMD_JUMP))
+		if(!s.equals(CMD_JUMP) && !s.equals(CMD_FIGHT))
 			return input.isKeyDown(keys.get(s));
 		return input.isKeyPressed(keys.get(s));
 	}
 
+	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta){
 		input = gc.getInput();
-		//Controls.input = gc.getInput();
-		Vec2 heroVec = hero.body.getWorldVector(new Vec2(-5.0f, 0)); // Get the vector that is used to apply force to the character
-		
+		// Get the vector that is used to apply force to the character
+		Vec2 heroVec = hero.body.getWorldVector(new Vec2(-1.0f, 0)); 
+		//Sets the linearDamping so that the body dosen't continue to go to the left, right or up
 		hero.body.setLinearDamping(2.0f);
-		//if(Controls.check(CMD_LEFT)){
+		//Tells the view to applay the animation for standing
+		if(!jump && doubleJump <1)
+			pa.standAnimation();
+		/*
+		 * All this if... is to handle the input from the keyboard
+		 */
 		if(check(CMD_LEFT)){
 			hero.body.applyForce(heroVec.mul(-1), hero.body.getPosition());
+			//if the charcter isn't jumping this will start the moving to the  left animation
+			if(!jump && doubleJump <1)
+				pa.leftAnimation();
 		}
-		//if(Controls.check(CMD_RIGHT)){
 		if(check(CMD_RIGHT)){
 			hero.body.applyForce(heroVec, hero.body.getPosition());
+			//if the charcter isn't jumping this will start the moving to the right animation
+			if(!jump && doubleJump <1)
+				pa.rightAnimation();
 		}
-		//if(Controls.check(CMD_JUMP)){
 		if(check(CMD_JUMP)){
-			jump = true;
+			//This will start the jump animation
+			pa.jumpAnimation();
+			if(doubleJump < 2)
+				jump = true;
+			doubleJump += 1;
 		}
-		//if(Controls.check(CMD_FIGHT)){
 		if(check(CMD_FIGHT)){
-			
+			// if you push the jump button it will start the animation and attack with it's weapon
+			pa.fightAnimation();
+			hero.attack();
+			hero.hurt(10);
 		}
 		if(jump){
-			hero.body.applyForce(hero.body.getWorldVector(new Vec2(.0f, 30.0f)), hero.body.getPosition());
+			hero.body.applyForce(hero.body.getWorldVector(new Vec2(.0f, 10.0f)), hero.body.getPosition());
+			//This is a jumping count and will control how far in to the air the character will move
 			jumpCount -= 1;
 			if(jumpCount <= 0 ){
 				jump =false;
@@ -99,15 +139,20 @@ public class CharacterController implements IEntityController{
 	}
 
 	@Override
-	public void init() {
-		
-	}
-
-	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) {
-		Vec2 tmp = hero.body.getPosition().add(new Vec2(-.5f,-.5f)).mul(50f);
-		g.drawAnimation(pa.listOfAnimation().get(0), tmp.x, tmp.y );
-		
+		if(!hero.isDead()){
+			Vec2 tmp = hero.body.getPosition().add(new Vec2(-hero.getWidth(),-hero.getHeight())).mul(hero.getTransfer());
+			g.drawAnimation(pa.getAnimation(), tmp.x, tmp.y );
+			g.drawString(hero.getName(), tmp.x , tmp.y- 40);
+			g.drawRect(tmp.x, tmp.y - 15, 100, 10);
+			g.fillRect(tmp.x, tmp.y - 15, hero.getHp(), 10);
+		}
 	}
-
+	
+	/**
+	 * A method to call when the character has hit the ground
+	 */
+	public void setGroundContact(){
+		doubleJump = 0;
+	}
 }
