5ce6af9f7d46c66ec50a503e8711ef612ade8f3c
elinljunggren <elin.l.ljunggren@gmail.com>
2013-05-03 13:12:37 +0200

    BulletController now Implements IEntityController

--- src/controller/BulletController.java
+++ src/controller/BulletController.java
@@ -1,5 +1,28 @@
 package controller;
 
-public class BulletController {
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
 
+import view.BulletView;
+import model.BulletModel;
+
+public class BulletController implements IEntityController{
+
+	private BulletModel model;
+	private BulletView view;
+	private World world;
+	private Vec2 position;
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) {
+		
+	}
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g) {
+		
+	}
+	
+	
 }
