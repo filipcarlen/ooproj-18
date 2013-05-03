22ef80bf4e73915b69d389eca882d6b8a14175e1
elinljunggren <elin.l.ljunggren@gmail.com>
2013-05-03 10:43:21 +0200

    BulletController implements IEntityController

--- src/controller/BulletController.java
+++ src/controller/BulletController.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class BulletController {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class BulletController implements IEntityController{
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) {
+				
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g) {
+		
+	}
 
 }
