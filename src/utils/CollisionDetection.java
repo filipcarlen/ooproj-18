3f17a4fbec65e8dc7a375f557c6bfe3d1dba2c20
Marcus Lagerstedt <lager93@gmail.com>
2013-05-03 13:26:46 +0200

    unnessesary varibles removed

--- src/utils/CollisionDetection.java
+++ src/utils/CollisionDetection.java
@@ -8,7 +8,6 @@ import model.IEntityModel;
 import org.jbox2d.callbacks.ContactImpulse;
 import org.jbox2d.callbacks.ContactListener;
 import org.jbox2d.collision.Manifold;
-import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.World;
 import org.jbox2d.dynamics.contacts.Contact;
 
@@ -18,15 +17,13 @@ public class CollisionDetection  implements ContactListener{
 	
 	List<IEntityModel> entityModels;
 	List<WorldShapes> terrain;
-	List<Body> terr;
 	PlayState playState;
 	World w;
 
-	public CollisionDetection(World w, List<IEntityModel> em, List<WorldShapes> ws, List<Body> b, PlayState ps){
+	public CollisionDetection(World w, List<IEntityModel> em, List<WorldShapes> ws, PlayState ps){
 		this.w = w;
 		this.entityModels = em;
 		this.terrain = ws;
-		this.terr = b;
 		this.playState =ps;
 		
 	}
