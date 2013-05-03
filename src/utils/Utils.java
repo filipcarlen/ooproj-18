7f7195f473d5ec24b72154b2b7578013bd678077
Filip Carlen <filipcarlen@gmail.com>
2013-05-03 11:01:53 +0200

    Merge branch 'master' of https://github.com/filipcarlen/ooproj-18
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,16 +1,18 @@
 package utils;
 
+import org.jbox2d.common.Vec2;
+
 public class Utils {
 	
 	public static float PIXEL_IN_METERS = 1f/30f;
 	public static float METER_IN_PIXELS = 30f;
 	
-	public static float pixelsToMeters(float pixels) {
-		return pixels*PIXEL_IN_METERS;
+	public static Vec2 pixelsToMeters(Vec2 pixels) {
+		return pixels.mul(PIXEL_IN_METERS);
 	}
 	
-	public static float metersToPixels(float meters) {
-		return meters*METER_IN_PIXELS;
+	public static Vec2 metersToPixels(Vec2 meters) {
+		return meters.mul(METER_IN_PIXELS);
 	}
 
 }
