3ec633e5998f4111439e4d4f712bfed3229311a1
elinljunggren <elin.l.ljunggren@gmail.com>
2013-05-06 16:23:18 +0200

    added SwordController and BulletModel again after the error..

---
+++ src/model/BulletModel.java
@@ -0,0 +1,88 @@
+package model;
+
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.World;
+
+import states.PlayState;
+import utils.Utils;
+
+/** A class representing a Bullet
+ * 
+ * @author elinljunggren
+ * @version 1.0 
+ */
+
+public class BulletModel implements IEntityModel{
+
+	private float range;
+	private int damage;
+	private Body body;
+	private World world;
+	
+	public final float RADIUS = 0.3f;
+
+	public BulletModel(World world, Vec2 heroPos, float range, int damage){
+		this.range = range;
+		this.damage = damage;
+		this.world = world;
+		init(heroPos);
+		
+	}
+	
+	public void init(Vec2 heroPos){
+		BodyDef bd = new BodyDef();
+		bd.type = BodyType.KINEMATIC;
+		bd.position.set(heroPos.x, heroPos.y);
+		
+		CircleShape cs = new CircleShape();
+		cs.m_radius = RADIUS;
+		
+		FixtureDef fd = new FixtureDef();
+		fd.shape = cs;
+		fd.density = 0.5f;
+		fd.friction = 0.3f;
+		fd.restitution = 0.2f;
+		
+		this.body = this.world.createBody(bd);
+		this.body.createFixture(fd);
+		this.body.setUserData(this);
+		this.body.setBullet(true);
+		this.body.shouldCollide(PlayState.getHeroModel().getBody());
+		
+	}
+	
+	public void fight(){
+		
+	}
+	
+	
+	public int getDamage(){
+		return this.damage;
+	}
+	public void setDamage(int damage){
+		this.damage = damage;
+	}
+	public float getRange(){
+		return this.range;
+	}
+	public void setRange(float range){
+		this.range = range;
+	}
+	@Override
+	public Vec2 getPosMeters() {
+		return this.body.getPosition();
+	}
+	@Override
+	public Vec2 getPosPixels() {
+		return Utils.metersToPixels(this.body.getPosition());
+	}
+	@Override
+	public Body getBody() {
+		return this.body;
+	}
+}
