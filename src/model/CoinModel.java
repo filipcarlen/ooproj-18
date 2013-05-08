b3609f349370dcb3386e34d11fa20d40bc9899d4
Filip Carlen <filipcarlen@gmail.com>
2013-05-08 14:07:14 +0200

    GemModel and CoinModel updated

--- src/model/CoinModel.java
+++ src/model/CoinModel.java
@@ -3,6 +3,26 @@ package model;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.World;
 
-public class CoinModel {
+public class CoinModel extends AbstractCollectibleModel {
+	
+	/** What value a collectible item holds (which points you get) */
+	private int value = 1;
+
+	public CoinModel(World w, Vec2 pixelPos) {
+		super(w, pixelPos);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setValue(int value) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/model/GemModel.java
+++ src/model/GemModel.java
@@ -3,5 +3,26 @@ package model;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.World;
 
-public class GemModel {
+
+public class GemModel extends AbstractCollectibleModel {
+	
+	/** What value a collectible item holds (which points you get) */
+	private int value = 5;
+
+	public GemModel(World w, Vec2 pixelPos) {
+		super(w, pixelPos);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setValue(int value) {
+		// TODO Auto-generated method stub
+		
+	}
 }
