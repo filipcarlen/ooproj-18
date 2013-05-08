d48ee3757976118131a86437374f75383e481481
linneaotterlind <linnea.otterlind@gmail.com>
2013-05-08 12:28:11 +0200

    Changed the CollisionDetection class substantionally. Also changed PlayState according to the changes in CollisionDetection.

--- src/utils/CollisionDetection.java
+++ src/utils/CollisionDetection.java
@@ -1,10 +1,10 @@
 package utils;
 
-import java.util.List;
-
-import map.WorldShapes;
+import model.BulletModel;
 import model.CollectibleModel;
-import model.IEntityModel;
+import model.MovingFoeModel;
+import model.StaticFoeModel;
+import model.SwordModel;
 
 import org.jbox2d.callbacks.ContactImpulse;
 import org.jbox2d.callbacks.ContactListener;
@@ -12,47 +12,130 @@ import org.jbox2d.collision.Manifold;
 import org.jbox2d.dynamics.World;
 import org.jbox2d.dynamics.contacts.Contact;
 
-import controller.HeroController;
 import model.HeroModel;
 
 import states.PlayState;
 
 public class CollisionDetection  implements ContactListener{
 	
-	List<IEntityModel> entityModels;
-	List<WorldShapes> terrain;
-	PlayState playState;
-	World w;
-	CollectibleModel cm;
+	World world;
 
-	public CollisionDetection(World w, List<IEntityModel> em, List<WorldShapes> ws, PlayState ps, CollectibleModel cm){
-		this.w = w;
-		this.entityModels = em;
-		this.terrain = ws;
-		this.playState =ps;
-		this.cm = cm;
+	public CollisionDetection(World world){
+		this.world = world;
 		
 	}
 	
 	@Override
-	public void beginContact(Contact c) {
-		if(c.m_fixtureB.m_body.getUserData() == PlayState.getHeroModel()){
-			if(c.m_fixtureA.m_body.getUserData() == EntityType.GROUND)
-				((HeroModel)c.m_fixtureB.m_body.getUserData()).setGroundContact();
+	public void beginContact(Contact contact) {
+		
+		Object objectA = contact.getFixtureA().getBody().getUserData();
+		Object objectB = contact.getFixtureB().getBody().getUserData();
+		
+		// Check if objectA of the collision is the hero and check what objectB is too 
+		// make the right changes.
+		if(objectA == PlayState.getHeroModel()){
 			
-			else if(c.m_fixtureA.m_body.getUserData() == PlayState.getCollectibleModel()){
-				PlayState.getCollectibleModel().killBody();
+			HeroModel heroModel = (HeroModel)objectB;
+			
+			if(objectB ==  EntityType.GROUND){
+				
+				heroModel.setGroundContact();
+			}
+	
+			else if(objectB instanceof CollectibleModel){
+				
+				System.out.println("hejB");
+			}
+			
+			else if(objectB instanceof StaticFoeModel) {
+				heroModel.hurt(((StaticFoeModel)objectB).getDamage());
+			}
+			
+			else if(objectB instanceof BulletModel) {
+				heroModel.hurt(((BulletModel)objectB).getDamage());
+			}
+			
+			else if(objectB instanceof SwordModel) {
+				heroModel.hurt(((SwordModel)objectB).getDamage());
 			}
 		}
-		if(c.m_fixtureA.m_body.getUserData() ==  PlayState.getHeroModel()){
-			if(c.m_fixtureB.m_body.getUserData() ==  EntityType.GROUND)
-				((HeroModel)c.m_fixtureA.m_body.getUserData()).setGroundContact();
+		
+		// Check if objectB of the collision is the hero and check what objectA is too 
+		// make the right changes.
+		if(objectB ==  PlayState.getHeroModel()){
 			
-			else if(c.m_fixtureB.m_body.getUserData() == PlayState.getCollectibleModel()){
-				PlayState.getCollectibleModel().killBody();
+			HeroModel heroModel = (HeroModel)objectB;
+			
+			if(objectA == EntityType.GROUND){
+				
+				heroModel.setGroundContact();
+			}
+	
+			else if(objectA instanceof CollectibleModel){
+				
+				System.out.println("hejA");
+			}
+			
+			else if(objectA instanceof StaticFoeModel) {
+				heroModel.hurt(((StaticFoeModel)objectA).getDamage());
+			}
+			
+			else if(objectA instanceof BulletModel) {
+				heroModel.hurt(((BulletModel)objectA).getDamage());
+			}
+			
+			else if(objectA instanceof SwordModel) {
+				heroModel.hurt(((SwordModel)objectA).getDamage());
 			}
 		}
-		System.out.println(c.m_fixtureA.m_body.getUserData() + "\n" + c.m_fixtureB.m_body.getUserData());
+		
+		// Check if objectB of the collision is a moving foe and check what objectA is too 
+		// make the right changes.
+		if(objectA instanceof MovingFoeModel){
+	
+			MovingFoeModel foeModel = (MovingFoeModel)objectA;
+			
+			if(objectB instanceof CollectibleModel){
+				
+			}
+			
+			else if(objectB instanceof StaticFoeModel) {
+				foeModel.hurt(((StaticFoeModel)objectA).getDamage());
+			}
+			
+			else if(objectB instanceof BulletModel) {
+				foeModel.hurt(((BulletModel)objectA).getDamage());
+			}
+			
+			else if(objectB instanceof SwordModel) {
+				foeModel.hurt(((SwordModel)objectA).getDamage());
+			}
+		}
+		
+		// Check if objectB of the collision is a moving foe and check what objectA is too 
+		// make the right changes.
+		if(objectB ==  PlayState.getHeroModel()){
+			
+			MovingFoeModel foeModel = (MovingFoeModel)objectA;
+	
+			if(objectA instanceof CollectibleModel){
+				
+			}
+			
+			else if(objectA instanceof StaticFoeModel) {
+				foeModel.hurt(((StaticFoeModel)objectA).getDamage());
+			}
+			
+			else if(objectA instanceof BulletModel) {
+				foeModel.hurt(((BulletModel)objectA).getDamage());
+			}
+			
+			else if(objectA instanceof SwordModel) {
+				foeModel.hurt(((SwordModel)objectA).getDamage());
+			}
+		}
+		
+		System.out.println(objectA + "\n" + objectB);
 	}
 
 	@Override
