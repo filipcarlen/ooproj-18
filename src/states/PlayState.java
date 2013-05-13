133abb364ecdda0422fef63c96338b322a084941
Marcus Lagerstedt <lager93@gmail.com>
2013-05-13 10:21:38 +0200

    RemoveController is updated so it can remove the right controller

--- src/states/PlayState.java
+++ src/states/PlayState.java
@@ -7,7 +7,6 @@ import map.WorldShapes;
 import model.CoinModel;
 import model.GemModel;
 import model.HeroModel;
-import model.ICollectibleModel;
 import model.IEntityModel;
 
 import org.jbox2d.common.Vec2;
@@ -16,7 +15,6 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
-import org.newdawn.slick.state.GameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 import utils.CollisionDetection;
@@ -33,6 +31,8 @@ public class PlayState extends BasicGameState{
 	HeroView pa;
 	WorldMap wm;
 	CollisionDetection cd;
+	int nbr= 0;
+	int stateID;
 	
 	
 	static ArrayList<IEntityModel> bodies = new ArrayList <IEntityModel>();
@@ -40,7 +40,7 @@ public class PlayState extends BasicGameState{
 	ArrayList<WorldShapes> terrain = new ArrayList <WorldShapes>();
 	
 	public PlayState(int id){
-		
+		stateID = id;
 	}
 	
 	public void loadCharacters(){
@@ -51,37 +51,32 @@ public class PlayState extends BasicGameState{
 		
 	}
 
-	
-	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		
 		// Creating the world
 		Vec2 gravity = new Vec2(0.0f, 9.8f);
 		world = new World(gravity);
-		world.step(1f/60f, 8, 3);
 		world.setAllowSleep(true);
 		world.setContinuousPhysics(true);
-		
+		cd = new CollisionDetection();
 		world.setContactListener(cd);
 		wm = new WorldMap(world, true, "test");
 		// Creating a character
 		hero = new HeroModel(world, "hero");
 		contHero = new HeroController(hero);
 		// Camera
-		bodies.add(new GemModel(world,new Vec2(500,340)));
+		bodies.add(new GemModel(world, new Vec2(500,240), 1));
 		controllers.add(new CollectibleController((GemModel)bodies.get(bodies.size()-1)));
-		bodies.add(new CoinModel(world,new Vec2(400, 340)));
+		bodies.add(new CoinModel(world,new Vec2(400, 340), 2));
 		controllers.add(new CollectibleController((CoinModel)bodies.get(bodies.size()-1)));
-		
-		cd = new CollisionDetection();
-		
 	}
 
-	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		wm.render(g);
 		try{
-			((CollectibleController)controllers.get(0)).render(gc, sbg, g);
+			for(int i = 0; i < controllers.size(); i++){
+				((CollectibleController)controllers.get(i)).render(gc, sbg, g);
+			}
 		}catch(IndexOutOfBoundsException e){}
 		try{
 			contHero.render(gc, sbg, g);
@@ -93,7 +88,6 @@ public class PlayState extends BasicGameState{
 				"\nBodyCount" + world.getBodyCount(), 100, 100);
 	}
 
-	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
 		world.step(1f/60f, 8, 3);
 		try{
@@ -101,18 +95,20 @@ public class PlayState extends BasicGameState{
 		}catch(NullPointerException e){} 
 	}
 
-	@Override
 	public int getID() {
-		return 1;
+		return stateID;
 	}
 	
 	public static HeroModel getHeroModel(){
 		return hero;
 	}
 	
-	public static void removeController(){
-		if(controllers.size() > 0){
-			controllers.remove(0);
+	public static void removeController(int id){
+		for(int i = 0; i < controllers.size(); i++){
+			if(((CollectibleController)controllers.get(i)).getID() == id){
+				controllers.remove(i);
+				return;
+			}
 		}
 	}
 	
