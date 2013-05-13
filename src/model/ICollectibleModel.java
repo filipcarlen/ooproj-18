48242d92ef4cca635617449dff22645084fbf978
Filip Carlen <filipcarlen@gmail.com>
2013-05-13 10:16:58 +0200

    Refactored ICollectibleModel. Extends IEntityModel

--- src/model/ICollectibleModel.java
+++ src/model/ICollectibleModel.java
@@ -2,11 +2,10 @@ package model;
 
 import org.jbox2d.common.Vec2;
 
-public interface ICollectibleModel {
+public interface ICollectibleModel extends IEntityModel {
 	
 	public int getValue();
 	public void setValue(int value);
-	public Vec2 getPosPixels();
 	public float getRadius();
 	public boolean bodyExists();
 	public void destroyBody();
