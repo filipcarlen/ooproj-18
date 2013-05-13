a04d1af9ebc87b49ff74a7ff1537f8e58e48334b
Marcus Lagerstedt <lager93@gmail.com>
2013-05-13 10:20:37 +0200

    GameApp change just because there is a error(init was called twice)

--- src/states/GameApp.java
+++ src/states/GameApp.java
@@ -6,35 +6,32 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class GameApp extends StateBasedGame{
+	public static final String gamename = "The Game";
 	public static final int MAINMENUSTATE = 0;
 	public static final int PLAYSTATE = 1;
 	public static final int OPTIONSSTATE = 2;
 	
-	private static String gamename = "The Game";
-	
 	public GameApp(String gamename) {
 		super(gamename);
-		GameApp.gamename = gamename;
-
-	}
-
-	public void initStatesList(GameContainer gc) throws SlickException {
 		this.addState(new MainMenuState(MAINMENUSTATE));
 		this.addState(new PlayState(PLAYSTATE));
 		this.addState(new OptionsState(OPTIONSSTATE));
-		this.getState(PLAYSTATE).init(gc, this);
 		this.enterState(MAINMENUSTATE);
+	}
+
+	public void initStatesList(GameContainer gc) throws SlickException {
+		//this.getState(MAINMENUSTATE).init(gc, this);
+		//this.getState(PLAYSTATE).init(gc, this);
+		//this.getState(OPTIONSSTATE).init(gc, this);
 	}
 	
 	public static void main(String []args){
-		AppGameContainer appgc;
 		try{
-			appgc = new AppGameContainer(new GameApp("The Game"));
+			AppGameContainer appgc = new AppGameContainer(new GameApp(gamename));
 			appgc.setDisplayMode(900, 600, false);
 			appgc.setTargetFrameRate(200);
 			appgc.start();
 		}catch(SlickException e){
 			e.printStackTrace();
 		}
-	} 
-}
+	} }
