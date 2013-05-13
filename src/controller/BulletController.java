8576f2bbaa62b3ee6ed4dc16c14f563ee56b0dcb
elinljunggren <elin.l.ljunggren@gmail.com>
2013-05-13 16:18:01 +0200

    Refactoring of Bullet and Gun

--- src/controller/BulletController.java
+++ src/controller/BulletController.java
@@ -24,7 +24,6 @@ public class BulletController implements IEntityController{
 
 	/** The model connected to this controller */
 	private List<BulletModel> models ;
-	private GunModel gunModel;
 	/** The view connected to this controller */
 	private ArrayList<BulletView> views = new ArrayList<BulletView>();;
 	/** The Distance the bullet has moved */
@@ -33,12 +32,11 @@ public class BulletController implements IEntityController{
 	private boolean isMoving;
 
 	
-	public BulletController(List<BulletModel> models, GunModel gunModel){
+	public BulletController(List<BulletModel> models){
 		this.models = models;
-		this.gunModel = gunModel;
-		for(int i = 0; i < models.length(); i++){
-			this.views.add(new BulletView(this.models.get(i)));
-		}
+		//for(int i = 0; i < models.length(); i++){
+		//	this.views.add(new BulletView(this.models.get(i)));
+		//}
 	}
 	
 	@Override
@@ -49,19 +47,20 @@ public class BulletController implements IEntityController{
 				// To get the distance we take the position from where the bullet was fired minus the current position
 				this.distance = Vec2.abs(this.models.get(i).getPosPixels().sub(this.models.get(i).getFirstPos()));
 		
-				if(!isMoving){
+				if(!models.get(i).isMoving()){
 					if(this.models.get(i).getNavigation() == Navigation.WEST){
 						this.models.get(i).getBody().applyForce(this.models.get(i).getBody().getWorldVector(new Vec2(-10.0f, 0.0f)), this.models.get(i).getBody().getPosition());
-						isMoving = true;
+						models.get(i).setMoving(true);
 					}else if(this.models.get(i).getNavigation() == Navigation.EAST){
 						this.models.get(i).getBody().applyForce(this.models.get(i).getBody().getWorldVector(new Vec2(10.0f, 0.0f)), this.models.get(i).getBody().getPosition());
-						isMoving = true;
+						models.get(i).setMoving(true);
 					}
 		
 				} else{
 					if(this.models.get(i).getRange() < distance.length()){
 						this.models.get(i).destroyEntity();
-						isMoving = false;
+						models.get(i).setMoving(false);
+						
 						models.get(i).getBody().setActive(false);
 					}
 				}
