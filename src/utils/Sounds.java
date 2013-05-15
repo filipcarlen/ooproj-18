5458151c7056b272dbcb05d8c19a1508fb387b7d
Filip Carlen <filipcarlen@gmail.com>
2013-05-15 18:13:36 +0200

    Created Sounds class. Contains all sounds.

---
+++ src/utils/Sounds.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
+
+import states.GameApp;
+import states.MainMenuState;
+
+public class Sounds{
+	
+	private static Sounds instance = null;
+	
+		public static String PATH = "res/Sounds/";
+		public Sound soundtrack;
+		
+		private Sounds() throws SlickException {
+			soundtrack = new Sound(PATH+"soundtrack.wav");
+		}
+		
+		public static Sounds getInstance() throws SlickException{
+			if(instance == null){
+				instance = new Sounds();
+			}
+			return instance;
+		}
+}
