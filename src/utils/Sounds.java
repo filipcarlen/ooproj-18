06f3724821812f863184c7c4aa710a6803bb5075
Filip Carlen <filipcarlen@gmail.com>
2013-05-16 16:33:57 +0200

    Sounds class updated with some new fresh sounds.

--- src/utils/Sounds.java
+++ src/utils/Sounds.java
@@ -10,9 +10,17 @@ public class Sounds{
 		public static String PATH = "res/Sounds/";
 		
 		public Sound soundtrack;
+		public Sound gunfire;
+		public Sound enemyHurt;
+		public Sound enemyDie;
+		public Sound musicIngame;
 		
 		private Sounds() throws SlickException {
 			soundtrack = new Sound(PATH+"soundtrack.wav");
+			gunfire = new Sound(PATH+"GE_KF7_Soviet.wav");
+			enemyHurt = new Sound(PATH+"enemyhurt.wav");
+			enemyDie = new Sound(PATH+"enemyDie.wav");
+			musicIngame = new Sound(PATH+"Musicingame.wav");
 		}
 		
 		public static Sounds getInstance() throws SlickException{
