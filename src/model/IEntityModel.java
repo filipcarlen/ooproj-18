e13bdc8ea9041aa138fa35939b2615731c03ce35
Marcus Lagerstedt <lager93@gmail.com>
2013-05-16 10:42:15 +0200

    Width and Height is now something incommen with all entities

--- src/model/IEntityModel.java
+++ src/model/IEntityModel.java
@@ -6,6 +6,27 @@ import org.jbox2d.dynamics.Body;
 public interface IEntityModel {
 	
 	/**
+	 * @return The body of this entity
+	 */
+	public Body getBody();
+	
+	/**
+	 * @return - The height of the hero in Meters
+	 */
+	public float getHeight();
+
+	/**
+	 * A method to easily connect the controller to model.
+	 * @return The ID of the entitymodel.
+	 */
+	public int getID();
+	
+	/**
+	 * @return - The width of the hero in Meter
+	 */
+	public float getWidth();
+
+	/**
 	 * @return The position of the center of this entity in meters.
 	 */
 	public Vec2 getPosMeters();
@@ -14,16 +35,5 @@ public interface IEntityModel {
 	 * @return The position of the top left corner of this entity in pixels.
 	 */
 	public Vec2 getPosPixels();
-	
-	/**
-	 * @return The body of this entity
-	 */
-	public Body getBody();
-	
-	/**
-	 * A method to easily connect the controller to model.
-	 * @return The ID of the entitymodel.
-	 */
-	public int getID();
 
 }
