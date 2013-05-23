09dbaefb85eeaf3f16a08a133285903528e6be98
Marcus Lagerstedt <lager93@gmail.com>
2013-05-23 10:53:22 +0200

    jumping is fixed

--- src/controller/HeroController.java
+++ src/controller/HeroController.java
@@ -25,7 +25,7 @@ import view.HeroView;
  * Version: 1.0 A controller for the Hero. This class will interact with the
  * View of the hero and the Model, to make the character move and fight.
  * 
- * @author Project Group 18 (Chalmers, 2013)
+ * @author Project Group 18 (Chalmers, 2213)
  * 
  */
 
@@ -37,15 +37,17 @@ public class HeroController implements IEntityController, ActionListener {
 	 * This is a count how many times i will apply force in the y axis, this is
 	 * use to simulate a real jump
 	 */
-	private int jumpCount = 18;
+	private int jumpCount = 26;
 
 	/*
 	 * This is a integer that will be set if somebody calls fight, and will
 	 * prevent that the standing animation will be active
 	 */
-	private int fightTimer = 20;
+	private Timer fightTimer;
 	
 	private boolean fight = false;
+	
+	private boolean move = false;
 
 	/* This is the Model */
 	private HeroModel model;
@@ -57,11 +59,13 @@ public class HeroController implements IEntityController, ActionListener {
 	private IEntityController controller;
 	
 	/* This timer controls how long time the hurt animations should continue*/
-	private Timer hurtTimer = new Timer(200, this);
+	private Timer hurtTimer = new Timer(220, this);
 
 	public HeroController(HeroModel hm) {
 		model = hm;
 		view = new HeroView(hm, model.getWeaponType());
+		fightTimer= new Timer(view.getAttackAnimation().getFrameCount()* 
+				view.getAttackAnimation().getDuration(0),this);
 		if(!model.isBodyCreated()){
 			/* Sets the dimension of the hero after the slimmest animation standing*/
 			model.setDimension(view.getWidth(), view.getHeight());
@@ -82,30 +86,28 @@ public class HeroController implements IEntityController, ActionListener {
 		/* Tells the Control which key i'm pressing */
 		Controls.getInstance().updateInput(gc.getInput());
 		
+		move = false;
+		
 		/* Get the vector that is used to apply force to the character */
-		Vec2 heroVec = model.getBody().getWorldVector(new Vec2(-1.0f, 0.0f));
+		Vec2 heroVec = model.getBody().getWorldVector(new Vec2(-1.2f, 0.0f));
 		
 		/*
 		 * Sets the linearDamping so that the body dosen't continue to go to the
 		 * left, right or up
 		 */
 		model.getBody().setLinearDamping(1.5f);
-
+	
 		if(model.isHurted() && !hurtTimer.isRunning()){
 			view.setAnimation(model.isHurtedFront()? HeroView.Movement.hurt: HeroView.Movement.hurtback, model.getDirection());
 			hurtTimer.start();
 		}
 		
-		/*
-		 * Tells the view to applay the animation for standing, if the character
-		 * isn't jumping or fighting
-		 */
-		if (!jump && !model.isFalling() && !model.isHurted()&& !fight)
-			view.setAnimation(HeroView.Movement.stand, model.getDirection());
 
+		
 		/* This if statements handels the inputs from the keyboard */
-		if (Controls.getInstance().check("left")) {
+		if (Controls.getInstance().check("left") && !Controls.getInstance().check("right")) {
 			model.setDirection(Navigation.WEST);
+			move = true;
 			model.getBody().applyForce(heroVec.mul(-1), model.getBody().getPosition());
 			/*
 			 * If the character isn't jumping this will start the moving to the
@@ -114,8 +116,9 @@ public class HeroController implements IEntityController, ActionListener {
 			if (!jump && !model.isFalling() && !model.isHurted()&& !fight)
 				view.setAnimation(HeroView.Movement.run, model.getDirection());
 		}
-		if (Controls.getInstance().check("right")) {
+		if (Controls.getInstance().check("right") && !Controls.getInstance().check("left")) {
 			model.setDirection(Navigation.EAST);
+			move = true;
 			model.getBody().applyForce(heroVec, model.getBody().getPosition());
 			/*
 			 * If the character isn't jumping this will start the moving to the
@@ -124,14 +127,27 @@ public class HeroController implements IEntityController, ActionListener {
 			if (!jump && !model.isFalling() && !model.isHurted()&& !fight)
 				view.setAnimation(HeroView.Movement.run, model.getDirection());
 		}
-		if (Controls.getInstance().check("jump") && !model.isFalling()) {
+		if (Controls.getInstance().check("jump")) {
+			move = true;
 			/* This will start the jump animation */
 			view.setAnimation(HeroView.Movement.jump, model.getDirection());
 			/* This check that we haven't jumped two times */
-			if (model.getDoubleJump() < 2)
+			if (model.getDoubleJump() < 2){
 				jump = true;
+				jumpCount = 26- (model.getDoubleJump()*15);
+			}
 			model.incrementJumps();
 		}
+		
+		/*
+		 * Tells the view to apply the animation for standing, if the character
+		 * isn't jumping or fighting
+		 */
+		if (!jump && !model.isFalling() && !model.isHurted()&& !fight && !move && !model.isInAir()){
+			view.setAnimation(HeroView.Movement.stand, model.getDirection());
+			model.getBody().setLinearDamping(5f);
+		}
+		
 		if (Controls.getInstance().check("fight")) {
 			/*
 			 * If you push the jump button it will start the animation and call
@@ -143,34 +159,26 @@ public class HeroController implements IEntityController, ActionListener {
 				else if(model.getWeaponType() == WeaponType.sword)
 					Sounds.getInstance().playSound(SoundType.SWORD);
 				fight = true;
+				fightTimer.start();
 				view.setAttackAnimation(model.getDirection());
 			}
 	
 		}
 		
-		if(fight){
-			fightTimer -=1;
-			if(fightTimer <= 0){
-				fightTimer = 20;
-				fight = false;
-			}
-		}
-
 		/*
 		 * This if Statement decrease the jumpcount so that it will reach 0 and
 		 * stop applying force in the y axis
 		 */
 		if (jump) {
 			model.getBody().applyForce(
-					model.getBody().getLocalVector(new Vec2(.0f, 10.0f)),
+					model.getBody().getLocalVector(new Vec2(.0f, 10f)),
 					model.getBody().getPosition());
 			jumpCount -= 1;
 			if (jumpCount <= 0) {
 				jump = false;
-				jumpCount = 18;
 			}
 		}
-		if(model.getBody().m_linearVelocity.y > 0.1f ){
+		if(model.getBody().m_linearVelocity.y > 0.001f ){
 			view.setAnimation(HeroView.Movement.fall, model.getDirection());
 			model.falling();
 		}
@@ -188,7 +196,8 @@ public class HeroController implements IEntityController, ActionListener {
 		g.drawString("\nCoins;" + model.getCoinAmount() +
 				"\nGems:" + model.getGemAmount() +
 				"\nScore:" + model.getScore() +
-				"\nKills:" + model.getKills(), gc.getWidth() -100, 2);
+				"\nKills:" + model.getKills() + 
+				"\nVector: " + model.getBody().getLinearDamping(), gc.getWidth() -400, 2);
 	}
 
 	@Override
@@ -198,7 +207,12 @@ public class HeroController implements IEntityController, ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		model.resetHurted();
-		hurtTimer.stop();
+		if(fight){
+			fightTimer.stop();
+			fight = false;
+		}else{
+			model.resetHurted();
+			hurtTimer.stop();
+		}
 	}
 }
