4482812209d010491d12305c2f05a3b1dd8ca3aa
Marcus Lagerstedt <lager93@gmail.com>
2013-05-25 17:56:44 +0200

    PreGameState updated so that you can choose a name, and if the input is worng it will say so

--- src/controller/states/PreGameState.java
+++ src/controller/states/PreGameState.java
@@ -1,25 +1,40 @@
 package controller.states;
 
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
 import org.jbox2d.common.Vec2;
+import org.newdawn.slick.AngelCodeFont;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 import utils.WeaponType;
 
-public class PreGameState extends BasicGameState {
+public class PreGameState extends BasicGameState implements ActionListener {
 	
 	private int stateID = -1;
 	private static final String PATH = "res/pre_game_menu/";
 	private Image background, sword, gun, title, play, playH, gunH, swordH, gunC, swordC, mainMenu, mainMenuH;
-	private Vec2 swordPos, gunPos, titlePos, playPos;
-	private boolean insidePlay, insideSword, insideGun, swordClicked, gunClicked;
+	private Vec2 swordPos, gunPos, titlePos, playPos, mainMenuPos;
+	private boolean insidePlay, insideSword, insideGun, swordClicked, gunClicked, insideMainMenu;
 	private static PreGameState instance = null;
 	private WeaponType weapon = WeaponType.FIST;
+	private TextField playerNameInput;
+	private AngelCodeFont acf;
+	private float resize;
+	private Timer choiseTimer = new Timer(1000, this);
 	
 	
 	private PreGameState(int stateID){
@@ -27,11 +42,15 @@ public class PreGameState extends BasicGameState {
 	}
 	
 	public void enter(GameContainer gc, StateBasedGame sbg)
-			throws SlickException {	
+			throws SlickException {
+		resize = gc.getWidth()/ 900;
 		titlePos = new Vec2(gc.getWidth()/2-title.getWidth()/2,10);
-		swordPos = new Vec2(gc.getWidth()*.25f-sword.getWidth()/2, gc.getHeight()*.5f-sword.getHeight()*.5f);
-		gunPos = new Vec2(gc.getWidth()*.75f-sword.getWidth()/2, gc.getHeight()*.5f-gun.getHeight()*.5f);
-		playPos = new Vec2(gc.getWidth()*.8f, gc.getHeight()*.8f);
+		swordPos = new Vec2(gc.getWidth()*.03f, gc.getHeight()*.2f);
+		gunPos = new Vec2(gc.getWidth()*.53f, gc.getHeight()*.2f);
+		playPos = new Vec2(gc.getWidth()*.85f, gc.getHeight()*.85f);
+		mainMenuPos = new Vec2(gc.getWidth()-playPos.x-play.getWidth(), gc.getHeight()*.85f);
+		playerNameInput =  new TextField(gc, new AngelCodeFont("res/font/fontwhite.fnt", "res/font/fontwhite_0.png"), 
+				(int)(gc.getWidth()*.5f), (int)(gc.getHeight()*.70f), 300, 50);
 	}
 	
 	public static PreGameState getInstance(){
@@ -54,7 +73,9 @@ public class PreGameState extends BasicGameState {
 		playH = new Image(PATH+"playH.png");
 		swordC = new Image(PATH+"swordClicked.png");
 		gunC = new Image(PATH+"gunClicked.png");
-		
+		mainMenu = new Image(PATH + "mainMenu.png");
+		mainMenuH = new Image(PATH + "mainMenuH.png");
+		acf = new AngelCodeFont("res/font/font.fnt", "res/font/font_0.png");
 	}
 
 	@Override
@@ -62,12 +83,13 @@ public class PreGameState extends BasicGameState {
 			throws SlickException {
 		background.draw(0, 0, gc.getWidth(), gc.getHeight());
 		title.draw(titlePos.x, titlePos.y);
-		
+		acf.drawString(playerNameInput.getX()-150, playerNameInput.getY(), "Player name: ");
+		playerNameInput.render(gc, g);
 		if(insideGun){
-			gunH.draw(gunPos.x,gunPos.y);
+			gunH.draw(gunPos.x,gunPos.y, gunH.getWidth()*resize, gunH.getHeight()*resize);
 		}
 		else{
-			gun.draw(gunPos.x, gunPos.y);
+			gun.draw(gunPos.x, gunPos.y, gun.getWidth()*resize, gun.getHeight()*resize);
 		}
 		if(insidePlay){
 			playH.draw(playPos.x, playPos.y);
@@ -76,17 +98,26 @@ public class PreGameState extends BasicGameState {
 			play.draw(playPos.x, playPos.y);
 		}
 		if(insideSword){
-			swordH.draw(swordPos.x, swordPos.y);
+			swordH.draw(swordPos.x, swordPos.y, swordH.getWidth()*resize, swordH.getHeight()*resize);
 		}
 		else{
-			sword.draw(swordPos.x, swordPos.y);
+			sword.draw(swordPos.x, swordPos.y, sword.getWidth()*resize, sword.getHeight()*resize);
+		}
+		if(insideMainMenu){
+			mainMenu.draw(mainMenuPos.x, mainMenuPos.y);
+		}else{
+			mainMenuH.draw(mainMenuPos.x, mainMenuPos.y);
 		}
 		if(swordClicked){
-			swordC.draw(swordPos.x, swordPos.y);
+			swordC.draw(swordPos.x, swordPos.y, swordC.getWidth()*resize, swordC.getHeight()*resize);
 		}
 		if(gunClicked){
-			gunC.draw(gunPos.x, swordPos.y);
-		}	
+			gunC.draw(gunPos.x, swordPos.y, gunC.getWidth()*resize, gunC.getHeight()*resize);
+		}
+		if(choiseTimer.isRunning()){
+			acf.drawString(gc.getWidth()*.25f, gc.getHeight()*.35f, 
+					"Your choise is not possible! \nEither you have not selected a weapon\nor your name is over 10 letters");
+		}
 	}
 
 	@Override
@@ -100,6 +131,7 @@ public class PreGameState extends BasicGameState {
 		insideGun = checkMouse(mouseX, mouseY, gunPos, gun);
 		insidePlay = checkMouse(mouseX, mouseY, playPos, play);
 		insideSword = checkMouse(mouseX, mouseY, swordPos, sword);
+		insideMainMenu = checkMouse(mouseX, mouseY, mainMenuPos, mainMenu);
 		
 		if(mouseClicked && insideGun){
 			weapon = WeaponType.GUN;
@@ -109,8 +141,17 @@ public class PreGameState extends BasicGameState {
 			gunClicked = true;
 		}
 		if(mouseClicked && insidePlay){
-			PlayState.getInstance().setWeaponInUse(weapon);
-			sbg.enterState(GameApp.PLAY_STATE);
+			if(weapon == WeaponType.FIST || playerNameInput.getText().length() > 10){
+				choiseTimer.start();
+			}else{
+				choiseTimer.stop();
+				PlayState.getInstance().setWeaponInUse(weapon);
+				if(playerNameInput.getText().length() == 0)
+					PlayState.getInstance().setPlayerName("Player");
+				else
+					PlayState.getInstance().setPlayerName(playerNameInput.getText());
+				sbg.enterState(GameApp.PLAY_STATE);
+			}
 		}
 		
 		if(mouseClicked && insideSword){
@@ -119,6 +160,9 @@ public class PreGameState extends BasicGameState {
 				gunClicked = false;
 			}
 			swordClicked = true;
+		}
+		if(mouseClicked && insideMainMenu){
+			sbg.enterState(GameApp.MAIN_MENU_STATE);
 		}
 	}
 
@@ -135,5 +179,10 @@ public class PreGameState extends BasicGameState {
 		else{
 			return false;
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		choiseTimer.stop();
 	}
 }
